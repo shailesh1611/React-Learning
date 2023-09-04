9f3ca2af25678dbc3205c97bcbeb4dca5da93715
shailesh1611 <shaileshsingh23423@gmail.com>
2023-09-04 22:43:22 +0530

    new module some complex tags added

---
+++ Inception/04 Some Complex Tags/app.js
@@ -0,0 +1,17 @@
+const element = React.createElement(
+    "div",
+    {id : "parent"},
+    React.createElement(
+        "div",
+        {id : "child"},
+        React.createElement(
+            "h1",
+            {},
+            "I'm h1 tag"
+        )
+    )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(element);
