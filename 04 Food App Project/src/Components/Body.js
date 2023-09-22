d1b49ed5c78dba305ea529ba95cb3610b07afe34
shailesh1611 <shaileshsingh23423@gmail.com>
2023-09-23 01:38:59 +0530

    filter feature added to app

--- 04 Food App Project/src/Components/Body.js
+++ 04 Food App Project/src/Components/Body.js
@@ -1,14 +1,33 @@
-import ResCard from './ResCard'
-import { resList } from '../utils/res_data';
+import ResCard from "./ResCard";
+import { resList } from "../utils/res_data";
+import { useState } from "react";
 
 const Body = () => {
+    const [dupResList, setDupResList] = useState(resList); // Hooks
     return (
         <div className="body">
-            <div className="search-bar">Search Bar</div>
-            <div className="res-Container"> 
-                {
-                    resList.map((res)=><ResCard props={res} key={res.info.id}></ResCard>)
-                }
+            <div className="filter">
+                <input type="number" placeholder="Filter By Rating" id="filter_box" />
+                <button
+                    className="btn_filter"
+                    onClick={() => {
+                        const inputValue = document.getElementById("filter_box").value;
+                        const num = +inputValue;
+                        if (num < 0 || num > 5) {
+                            alert("Please! filter between 0 to 5");
+                        } 
+                        else {
+                            const list = resList.filter((res) => res.info.avgRating >= num);
+                            setDupResList(list);
+                        }
+                    }}> Filter
+                </button>
+            </div>
+
+            <div className="res-Container">
+                {dupResList.map((res) => (
+                    <ResCard props={res} key={res.info.id}></ResCard>
+                ))}
             </div>
         </div>
     );
