1840279e45c2d04398838174268ec780ac2e6e0c
shailesh1611 <shaileshsingh23423@gmail.com>
2023-10-06 23:46:23 +0530

    login button added in header

--- 04 Food App Project/src/Components/Header.js
+++ 04 Food App Project/src/Components/Header.js
@@ -1,21 +1,32 @@
 import logo from '../../assets/images/logo.png';
-const Header = () => (
-    <div className="header">
+import {useState} from "react";
+const Header = () => {
+    const [login,setLogin] = useState("Login");
+    return (
+        <div className="header">
 
-        <div className="logo">
-            <img src={logo}/>
+            <div className="logo">
+                <img src={logo}/>
+            </div>
+
+            <div className = "nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                    <button className="btn_login_logout" onClick={
+                        () => {
+                            if(login === "Login") setLogin("Logout");
+                            else setLogin("Login");
+                        }
+                    }>{login}</button>
+                </ul>
+            </div>
+
         </div>
-
-        <div className = "nav-items">
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact Us</li>
-                <li>Cart</li>
-            </ul>
-        </div>
-
-    </div>
-);
+    );
+    
+};
 
 export default Header;
